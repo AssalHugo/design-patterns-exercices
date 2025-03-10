d6a0cf982af26926c9a0bc28056555cd7cfb1680
hugo assal <hugo.assal1@etu.univ-lorraine.fr>
2025-03-10 15:08:14 +0100

    builder fait

--- builder/app/QueryBuilderInterface.php
+++ builder/app/QueryBuilderInterface.php
@@ -1,5 +1,12 @@
 <?php
 
-# TODO: Créer une classe QueryBuilder en utilisant le design pattern Builder
-
 namespace App;
+
+interface QueryBuilderInterface
+{
+    public function select(string $fields): QueryBuilderInterface;
+    public function from(string $table): QueryBuilderInterface;
+    public function where(string $condition): QueryBuilderInterface;
+    public function orderBy(string $order): QueryBuilderInterface;
+    public function getQuery(): string;
+}
--- builder/public/index.php
+++ builder/public/index.php
@@ -1,7 +1,14 @@
 <?php
-require('../vendor/autoload.php');
+require __DIR__ . '/../vendor/autoload.php';
 
 
-# TODO: Creer un QueryBuilder
-# Ecrire une requête en chainant des methodes
-# Afficher la requête
+use App\MySQLQueryBuilder;
+
+$query = (new MySQLQueryBuilder())
+    ->select('prenom, nom, age')
+    ->from('users')
+    ->where('age > 18')
+    ->orderBy('age')
+    ->getQuery();
+
+var_dump($query);
